9c20e59ef8f260d0224e02ad62c6910272629622
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-10 00:20:14 +0700

    mmebuat SecondActivity.kt dan mengubah MainActivity.kt dengan menambahkan intent supaya dapat berpindah dari main ke second

--- app/src/main/java/com/example/intent_app/MainActivity.kt
+++ app/src/main/java/com/example/intent_app/MainActivity.kt
@@ -1,37 +1,33 @@
 package com.example.intent_app
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intent_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivityLifecycle"
+    private lateinit var binding: ActivityMainBinding
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btnToSecondActivity.setOnClickListener {
+                val intentToSecondActivity =
+                    Intent(this@MainActivity, SecondActivity::class.java)
+                startActivity(intentToSecondActivity)
+            }
+        }
+
         Log.d(TAG, "onCreate: dipanggil")
-    }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart: dipanggil")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume: dipanggil")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause: dipanggil")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop: dipanggil")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy: dipanggil")
     }
 }
