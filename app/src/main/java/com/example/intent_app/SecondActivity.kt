74332c51217bfbbf85a5e5d63fdd569b9a103d28
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-10 01:55:46 +0700

    mengenal ActivityResult

--- app/src/main/java/com/example/intent_app/SecondActivity.kt
+++ app/src/main/java/com/example/intent_app/SecondActivity.kt
@@ -1,21 +1,46 @@
 package com.example.intent_app
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intent_app.MainActivity.Companion.EXTRA_NAME
 import com.example.intent_app.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result ->
+            // Memeriksa result code
+            if (result.resultCode == Activity.RESULT_OK) {
+                // Mengambil data Intent
+                val data = result.data
+                // Mendapatkan alamat dari data Intent
+                val name = data?.getStringExtra(EXTRA_NAME)
+                val address = data?.getStringExtra(EXTRA_ADDRESS)
+                // Menetapkan teks di TextView
+                binding.txtName.text = "$name beralamat di $address"
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val name = intent.getStringExtra(MainActivity.EXTRA_NAME)
         with(binding){
-            txtName.text = name
+            btnToThirdActivity.setOnClickListener {
+                val intent = Intent(this@SecondActivity, ThirdActivity::class.java)
+                    .apply { putExtra(EXTRA_NAME,name) }
+                launcher.launch(intent)
+            }
         }
+    }
+    companion object{
+        const val EXTRA_ADDRESS = "extra_address"
     }
 }
---
+++ app/src/main/java/com/example/intent_app/ThirdActivity.kt
@@ -0,0 +1,34 @@
+package com.example.intent_app
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.intent_app.MainActivity.Companion.EXTRA_NAME
+import com.example.intent_app.SecondActivity.Companion.EXTRA_ADDRESS
+import com.example.intent_app.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val name = intent.getStringExtra(EXTRA_NAME)
+        with(binding) {
+            btnToSecondActivity.setOnClickListener {
+                val resultIntent = Intent()
+                // Memasukkan alamat ke Intent
+                resultIntent.putExtra(EXTRA_ADDRESS, edtAddress.text.toString())
+                resultIntent.putExtra(EXTRA_NAME, name)
+                // Menetapkan result code dan data Intent
+                setResult(Activity.RESULT_OK, resultIntent)
+                // Menyelesaikan aktivitas
+                finish()
+            }
+        }
+    }
+}
